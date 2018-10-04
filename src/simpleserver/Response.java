d8ddf334f41a290a666b45c5bf6001ac1c415086
Vanita <vrani@mail.sfsu.edu>
2018-10-03 19:11:17 -0700

    response

--- src/simpleserver/Response.java
+++ src/simpleserver/Response.java
@@ -1,23 +1,4 @@
 package simpleserver;
-//interface iData {
-//
-//}
-//
-//class user implements iData {
-//
-//}
-//
-//class Post implements iData {
-//
-//}
-//
-//public class Response {
-//    User[] users;
-//    Posts[] posts;
-//    public String status;
-//    public int entries;
-//    iData[] data;
-//}
 
 class Response {
     Data [] data;
@@ -26,6 +7,7 @@ class Response {
     int entries;
 
     public void setData(Data[] data) {
+
         this.data = data;
     }
 }
